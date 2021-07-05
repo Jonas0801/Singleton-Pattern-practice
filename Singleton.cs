97f466dd00ded28bba08c105aed297de3cfa75d4
Jonas <46156216+Jonas0801@users.noreply.github.com>
2021-07-05 14:38:47 +0800

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+namespace DesignPattern
+{
+    public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        private static bool isShuttingDown = false;
+        private static object lockObject = new object();
+        private static T instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (isShuttingDown)
+                {
+                    Debug.LogWarning($"[Singleton] Instance '{typeof(T)}' already destroyed. Returning null");
+                    return null;
+                }
+
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        instance = (T)FindObjectOfType(typeof(T));
+
+                        if (instance == null)
+                        {
+                            var singletonObject = new GameObject();
+                            instance = singletonObject.AddComponent<T>();
+                            singletonObject.name = typeof(T).Name.ToString() + "(Singleton)";
+                        }
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            isShuttingDown = false;
+        }
+
+        private void OnApplicationQuit()
+        {
+            isShuttingDown = false;
+        }
+    }
+}
